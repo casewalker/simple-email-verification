4e70393a5ebe11d8b988db851a54d928babb6a34
Case Walker <case.b.walker@gmail.com>
2024-03-02 17:09:10 -0800

    Update packages, clean up code slightly, remove unnecessary serverless template

--- handler.ts
+++ handler.ts
@@ -5,13 +5,9 @@ export const hello = async (
 ): Promise<APIGatewayProxyResult> => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: "Go Serverless v3.0! Your function executed successfully!",
+      input: event,
+    }),
   };
 };
